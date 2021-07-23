5dc3242f857b5848481698170ba04b820ec80b2e
Martin Maul <martin.maul@hs-weingarten.de>
2021-07-23 20:01:17 +0200

    fix

--- scripts/tableLoader.js
+++ scripts/tableLoader.js
@@ -1,5 +1,5 @@
-
-function loadData(dataStart,dataEnd) {
+var json = new Array();
+function loadData(dataStart, dataEnd) {
 
     //const tableContent = new Array();
     const request = new XMLHttpRequest();
@@ -9,9 +9,9 @@ function loadData(dataStart,dataEnd) {
     request.onload = () => {
         try {
             //JSON Daten werden in die const 'json' eingespeichert  
-            const json = JSON.parse(request.responseText);
+            json = JSON.parse(request.responseText);
             loadPagination(json, dataStart, dataEnd);
-            
+
 
         } catch (e) {
             console.warn("Could not load Data.")
@@ -20,31 +20,26 @@ function loadData(dataStart,dataEnd) {
     request.send();
 }
 
+function appendData(pages) {
 
-function appendData(json) {
-    
     var tableRef = document.getElementById('data-table')
 
     var rows = document.getElementById('rows');
 
-    while ( tableRef.hasChildNodes()){
-        tableRef.removeChild(tableRef.firstChild);
-    }
 
-    for (var i in json) {                       // ggf. hier mal ein Template festlegen
+
+    for (var i in pages) {                       // ggf. hier mal ein Template festlegen
 
         var newRow = tableRef.insertRow(-1);
 
-        for (var j in json[i]) {
+        for (var j in pages[i]) {
             if (j == 'token') {
                 break;
             }
             var cell = newRow.insertCell(-1);
-            cell.innerHTML = json[i][j];
+            cell.innerHTML = pages[i][j];
         }
     }
-
-   // loadPagination(json);
 
 }
 
@@ -52,15 +47,13 @@ function loadPagination(json, dataStart, dataEnd) {
     console.log(dataStart);
     console.log(dataEnd);
 
-    
     var pages = new Array();
-    var start = dataStart;
-    var activePageSize = dataEnd;
-    pages = json.slice(start,activePageSize);
 
-       console.log(pages);
+    pages = json.slice(dataStart, dataEnd);
 
-       appendData(pages);
+    console.log(pages);
+
+    appendData(pages);
 
 }
 
